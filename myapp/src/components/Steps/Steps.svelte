81ef006bde4ac5c1721bae9109466a2b18c4d448
David <gidlos55@gmail.com>
2025-10-15 18:02:11 +0300

    feat: Добавит web-отображение для блока steps

--- myapp/src/components/Steps/Steps.svelte
+++ myapp/src/components/Steps/Steps.svelte
@@ -5,28 +5,28 @@
 
 <section class="steps-section">
     <div class="steps-header">
-        <h2 class="steps-title">3 easy steps to becoming a trader</h2>
+        <h2 class="steps-title">3 easy steps<br> to becoming a trader</h2>
         <p class="steps-subtitle">How to start?</p>
     </div>
     <ul class="steps-list">
-        <li class="steps-item">
-            <span class="steps-item-header white">
+        <li class="steps-item white">
+            <span class="steps-item-header">
                 <p class="steps-item-number">Step 1</p>
                 <img src={arrow} alt="arrow" />
             </span>
-            <p class="steps-item-text">Open a trading account in a few seconds and start trading</p>
+            <p class="steps-item-text">Open a trading account<br> in a few seconds</p>
         </li>
          <li class="steps-item">
-            <span class="steps-item-header black">
+            <span class="steps-item-header">
                 <p class="steps-item-number">Step 2</p>
             </span>
-            <p class="steps-item-text">Master your skills with a $10.000 demo account</p>
+            <p class="steps-item-text">Master your skills with<br> a $10.000 demo account</p>
         </li>
         <li class="steps-item">
-            <span class="steps-item-header black">
+            <span class="steps-item-header">
                 <p class="steps-item-number">Step 3</p>
             </span>
-            <p class="steps-item-text">Make a deposit and start trading with real funds</p>
+            <p class="steps-item-text">Make a deposit and start<br> trading with real funds</p>
         </li>
     </ul>
 </section>
