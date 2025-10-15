871f91212aa3a9bc70799e3b52ed42acd11c5344
David <gidlos55@gmail.com>
2025-10-15 20:25:49 +0300

    feat: Добавит web-отображение для блока Registration и Reviews

--- myapp/src/components/Reviews/Reviews.svelte
+++ myapp/src/components/Reviews/Reviews.svelte
@@ -4,24 +4,26 @@
 
 <section class="reviews-section">
     <div class="reviews-header">
-        <h2 class="reviews-title">Don't take our word for it</h2>
+        <h2 class="reviews-title">Don't take our<br> word for it</h2>
         <p class="reviews-subtitle">Reviews</p>
     </div>
-    <div class="reviews-cards-container">
-        <div class="reviews-card">
+    <ul class="reviews-cards-container">
+        <li class="reviews-card">
             <p class="reviews-card-text">The app is fast and does not freeze even during active trading sessions. Everything is clear and understandable. I think you've done a great job on these 2 points: platform power and usability. Thanks for a great product!</p>
-            <datetime class="reviews-card-date" datetime="2021-12-12">December 12, 2021</datetime>
+            <time class="reviews-card-date" datetime="2021-12-12">December 12, 2021</time>
             <p class="reviews-card-user">Jorge2221</p>
-        </div>
-        <div class="reviews-card">
-            <p class="reviews-card-text">TI appreciate fair trading conditions, tight spreads and especially withdrawals via Payretailers. It would be cool to add more local payment systems, and then it would be perfect!</p>
-            <datetime class="reviews-card-date" datetime="2022-18-01">January 18, 2022</datetime>
+        </li>
+        <li class="reviews-card">
+            <p class="reviews-card-text">I appreciate fair trading conditions, tight spreads and especially withdrawals via Payretailers. It would be cool to add more local payment systems, and then it would be perfect!</p>
+            <time class="reviews-card-date" datetime="2022-01-18">January 18, 2022</time>
             <p class="reviews-card-user">Madsen_invest</p>
-        </div>
-        <div class="reviews-card">
-            <p class="reviews-card-text">Really great experience w/@exnova from day 1. I liked the clear interface and the interesting choice of assets. Additional tools and hints help make decisions faster, which is great!</p>
-            <datetime class="reviews-card-date" datetime="2021-24-12">December 24, 2021</datetime>
+        </li>
+        <li class="reviews-card">
+            <p class="reviews-card-text">Really great experience w/<br>@exnova from day 1. I liked the clear interface and the interesting choice of assets. Additional tools and hints help make decisions faster, which is great!</p>
+            <time class="reviews-card-date" datetime="2021-12-24">December 24, 2021</time>
             <p class="reviews-card-user">dannyofficial</p>
-        </div>
-    </div>
+        </li>
+        <li><img src="/images/reviews-block/arrow-right-white.svg" alt=""></li>
+    </ul>
+    
 </section>
